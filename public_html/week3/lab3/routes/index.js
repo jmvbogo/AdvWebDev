992949e50cfcac3d89f35cf4ca6f3c24a3c859b7
Jordan-Michael <Jordan-Michael@DESKTOP-NHMUS10.fios-router.home>
2017-10-28 00:31:15 -0400

    Continued week3 lab3, mostly test scripts.

--- public_html/week3/lab3/routes/index.js
+++ public_html/week3/lab3/routes/index.js
@@ -3,9 +3,25 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Rando Colors' });
+    
+    var size = req.body.gridSize;
+    
+        console.log(req.body.gridSize);
+    
+      var cells = [
+        {colors: 'testclr'+size},
+        {colors: 'testclr2'},
+        {colors: 'testclr3'},
+        {colors: 'testclr4'},
+        {colors: 'testclr5'},
+        {colors: 'testclr6'},
+        {colors: 'testclr7'},
+        {colors: 'testclr8'},
+        {colors: 'testclr9'}
+    ];
+    res.render('index', {title: "Rando Colors", results : cells});
 });
 
 
-module.exports = router;
 
+module.exports = router;
